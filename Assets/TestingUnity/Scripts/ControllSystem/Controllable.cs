7069619cd1dbfbb9e35d821a60335106e576de8b
Dnelre-Slin <nilsheggem@hotmail.com>
2021-06-05 17:37:48 +0200

    Moved ActionTypeHandler into ControllSystem folder

--- Assets/TestingUnity/Scripts/ControllSystem/Controllable.cs
+++ Assets/TestingUnity/Scripts/ControllSystem/Controllable.cs
@@ -4,31 +4,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-// public class ActionTypeHandler
-// {
-//     [System.Flags]
-//     public enum ActionType
-//     {
-//         None = 0,
-//         Started = 1,
-//         Performed = 2,
-//         Canceled = 4
-//     }
-
-//     static public bool IsStarted(ActionType type)
-//     {
-//         return (ActionType.Started & type) == ActionType.Started;
-//     }
-//     static public bool IsPerformed(ActionType type)
-//     {
-//         return (ActionType.Performed & type) == ActionType.Performed;
-//     }
-//     static public bool IsCanceled(ActionType type)
-//     {
-//         return (ActionType.Canceled & type) == ActionType.Canceled;
-//     }
-// }
-
 public class Controllable : MonoBehaviour, IControllable
 {
     public static event Action<Controllable> OnNewControlled;
