ba5efde27ff347f874df94d8b63ccdb3f3f5729d
Dnelre-Slin <nilsheggem@hotmail.com>
2021-06-06 17:35:37 +0200

    Create a unique function for coping each collider type, rather than using all properties copy

--- Assets/InertialDampeningSystem/Scripts/Helper/CopyColliderHelper.cs
+++ Assets/InertialDampeningSystem/Scripts/Helper/CopyColliderHelper.cs
@@ -4,9 +4,92 @@ using UnityEngine;
 
 public static class CopyColliderHelper
 {
+    public static BoxCollider CopyBoxCollider(BoxCollider originalCollider, GameObject copyGameObject)
+    {
+        BoxCollider copyCollider = copyGameObject.AddComponent<BoxCollider>();
+
+        // Copy box properties
+        copyCollider.isTrigger = originalCollider.isTrigger;
+        copyCollider.sharedMaterial = originalCollider.sharedMaterial;
+        copyCollider.center = originalCollider.center;
+        copyCollider.size = originalCollider.size;
+
+        return copyCollider;
+    }
+    public static SphereCollider CopySphereCollider(SphereCollider originalCollider, GameObject copyGameObject)
+    {
+        SphereCollider copyCollider = copyGameObject.AddComponent<SphereCollider>();
+
+        // Copy sphere properties
+        copyCollider.isTrigger = originalCollider.isTrigger;
+        copyCollider.sharedMaterial = originalCollider.sharedMaterial;
+        copyCollider.center = originalCollider.center;
+        copyCollider.radius = originalCollider.radius;
+
+        return copyCollider;
+    }
+    public static CapsuleCollider CopyCapsuleCollider(CapsuleCollider originalCollider, GameObject copyGameObject)
+    {
+        CapsuleCollider copyCollider = copyGameObject.AddComponent<CapsuleCollider>();
+
+        // Copy capsule properties
+        copyCollider.isTrigger = originalCollider.isTrigger;
+        copyCollider.sharedMaterial = originalCollider.sharedMaterial;
+        copyCollider.center = originalCollider.center;
+        copyCollider.radius = originalCollider.radius;
+        copyCollider.height = originalCollider.height;
+        copyCollider.direction = copyCollider.direction;
+
+        return copyCollider;
+    }
+    public static MeshCollider CopyMeshCollider(MeshCollider originalCollider, GameObject copyGameObject)
+    {
+        MeshCollider copyCollider = copyGameObject.AddComponent<MeshCollider>();
+
+        // Copy capsule properties
+        copyCollider.convex = originalCollider.convex;
+        copyCollider.isTrigger = originalCollider.isTrigger;
+        copyCollider.cookingOptions = originalCollider.cookingOptions;
+        copyCollider.sharedMaterial = originalCollider.sharedMaterial;
+        copyCollider.sharedMesh = originalCollider.sharedMesh;
+
+        return copyCollider;
+    }
+    public static TerrainCollider CopyTerrainCollider(TerrainCollider originalCollider, GameObject copyGameObject)
+    {
+        TerrainCollider copyCollider = copyGameObject.AddComponent<TerrainCollider>();
+
+        // Copy capsule properties
+        copyCollider.isTrigger = originalCollider.isTrigger;
+        copyCollider.sharedMaterial = originalCollider.sharedMaterial;
+        copyCollider.terrainData = originalCollider.terrainData;
+
+        return copyCollider;
+    }
+
     public static T CopyCollider<T>(T originalCollider, GameObject copyGameObject) where T : Collider
     {
-        return copyGameObject.AddComponent<T>(originalCollider);
+        if (originalCollider is BoxCollider)
+        {
+            return CopyColliderHelper.CopyBoxCollider(originalCollider as BoxCollider, copyGameObject) as T;
+        }
+        else if (originalCollider is SphereCollider)
+        {
+            return CopyColliderHelper.CopySphereCollider(originalCollider as SphereCollider, copyGameObject) as T;
+        }
+        else if (originalCollider is CapsuleCollider)
+        {
+            return CopyColliderHelper.CopyCapsuleCollider(originalCollider as CapsuleCollider, copyGameObject) as T;
+        }
+        else if (originalCollider is MeshCollider)
+        {
+            return CopyColliderHelper.CopyMeshCollider(originalCollider as MeshCollider, copyGameObject) as T;
+        }
+        else if (originalCollider is MeshCollider)
+        {
+            return CopyColliderHelper.CopyMeshCollider(originalCollider as MeshCollider, copyGameObject) as T;
+        }
+        throw new UnityException("Type : " + typeof(T) + " is not currently supported");
     }
 
     public static GameObject CopyCollidersToFromGameObject(GameObject originalGameObject, GameObject copyGameObject,
