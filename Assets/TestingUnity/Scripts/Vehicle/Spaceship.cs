ac239ef803a15ac553eb423eccef3a97caeb31f1
Dnelre-Slin <nilsheggem@hotmail.com>
2021-06-06 03:19:29 +0200

    Separated logic for spaceship and spaceship controller

---
+++ Assets/TestingUnity/Scripts/Vehicle/Spaceship.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Spaceship : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 thurstSpeeds = new Vector3(100f, 100f, 100f);
+
+    [SerializeField]
+    private float pitchYawSensitivity = 10f;
+    [SerializeField]
+    private float rollSensitivity = 40f;
+
+    private Rigidbody rgbd;
+    private Vector3 thrustVelocity;
+    private Vector3 turnVelocity;
+
+
+    void Start()
+    {
+        rgbd = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        HandleThrust();
+    }
+    void Update()
+    {
+        HandleTurn();
+    }
+
+    public void OnThrustForward(InputAction.CallbackContext context)
+    {
+        this.thrustVelocity.z = context.ReadValue<float>();
+    }
+    public void OnThrustRight(InputAction.CallbackContext context)
+    {
+        this.thrustVelocity.x = context.ReadValue<float>();
+    }
+    public void OnThrustUp(InputAction.CallbackContext context)
+    {
+        this.thrustVelocity.y = context.ReadValue<float>();
+    }
+    public void OnTurnPitchYaw(InputAction.CallbackContext context)
+    {
+        Vector2 input = context.ReadValue<Vector2>();
+        this.turnVelocity = new Vector3(-input.y * this.pitchYawSensitivity, input.x * this.pitchYawSensitivity, this.turnVelocity.z);
+    }
+    public void OnTurnRoll(InputAction.CallbackContext context)
+    {
+        this.turnVelocity.z = -context.ReadValue<float>() * this.rollSensitivity;
+    }
+
+    public void OnFullStop()
+    {
+        this.thrustVelocity = Vector3.zero;
+        this.turnVelocity = Vector3.zero;
+    }
+
+    void HandleThrust()
+    {
+        this.rgbd.velocity = this.transform.TransformDirection(Vector3.Scale(this.thrustVelocity, this.thurstSpeeds) * Time.fixedDeltaTime);
+    }
+
+    void HandleTurn()
+    {
+        this.transform.Rotate(this.turnVelocity * Time.deltaTime);
+    }
+}
--- Assets/TestingUnity/Scripts/Vehicle/SpaceshipController.cs
+++ Assets/TestingUnity/Scripts/Vehicle/SpaceshipController.cs
@@ -1,92 +1,37 @@
-﻿using System.Collections;
+﻿#if (ENABLE_INPUT_SYSTEM)
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(Rigidbody), typeof(InputSystemControlScheme), typeof(Controllable))]
+[RequireComponent(typeof(Spaceship), typeof(InputSystemControlScheme), typeof(Controllable))]
 public class SpaceshipController : MonoBehaviour
 {
+    private Spaceship spaceship;
     private Controllable controllable;
     private InputSystemControlScheme controlScheme;
-    private Rigidbody rgbd;
 
-    private Vector3 thrustVelocity;
-    private Vector3 turnVelocity;
-
-    [SerializeField]
-    private Vector3 thurstSpeeds = new Vector3(100f, 100f, 100f);
-
-    [SerializeField]
-    private float pitchYawSensitivity = 10f;
-    [SerializeField]
-    private float rollSensitivity = 40f;
-
-    // Start is called before the first frame update
     void Start()
     {
-        rgbd = GetComponent<Rigidbody>();
+        this.spaceship = GetComponent<Spaceship>();
 
         this.controllable = GetComponent<Controllable>();
         this.controlScheme = GetComponent<InputSystemControlScheme>();
 
         this.controlScheme.AddActionMap("Spaceship");
-        this.controlScheme.AddAction("Spaceship", "ThrustForward", ActionTypeHandler.ActionType.Performed | ActionTypeHandler.ActionType.Canceled, OnThrustForward);
-        this.controlScheme.AddAction("Spaceship", "ThrustRight", ActionTypeHandler.ActionType.Performed | ActionTypeHandler.ActionType.Canceled, OnThrustRight);
-        this.controlScheme.AddAction("Spaceship", "ThrustUp", ActionTypeHandler.ActionType.Performed | ActionTypeHandler.ActionType.Canceled, OnThrustUp);
-        this.controlScheme.AddAction("Spaceship", "TurnPitchYaw", ActionTypeHandler.ActionType.Performed | ActionTypeHandler.ActionType.Canceled, OnTurnPitchYaw);
-        this.controlScheme.AddAction("Spaceship", "TurnRoll", ActionTypeHandler.ActionType.Performed | ActionTypeHandler.ActionType.Canceled, OnTurnRoll);
+        this.controlScheme.AddAction("Spaceship", "ThrustForward", ActionTypeHandler.ActionType.Performed | ActionTypeHandler.ActionType.Canceled, this.spaceship.OnThrustForward);
+        this.controlScheme.AddAction("Spaceship", "ThrustRight", ActionTypeHandler.ActionType.Performed | ActionTypeHandler.ActionType.Canceled, this.spaceship.OnThrustRight);
+        this.controlScheme.AddAction("Spaceship", "ThrustUp", ActionTypeHandler.ActionType.Performed | ActionTypeHandler.ActionType.Canceled, this.spaceship.OnThrustUp);
+        this.controlScheme.AddAction("Spaceship", "TurnPitchYaw", ActionTypeHandler.ActionType.Performed | ActionTypeHandler.ActionType.Canceled, this.spaceship.OnTurnPitchYaw);
+        this.controlScheme.AddAction("Spaceship", "TurnRoll", ActionTypeHandler.ActionType.Performed | ActionTypeHandler.ActionType.Canceled, this.spaceship.OnTurnRoll);
         this.controlScheme.AddAction("Spaceship", "ExitVehicle", ActionTypeHandler.ActionType.Performed, OnExitVehicle);
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        HandleThrust();
-        HandleTurn();
-    }
-
-    public void OnThrustForward(InputAction.CallbackContext context)
-    {
-        this.thrustVelocity.z = context.ReadValue<float>();
-    }
-    public void OnThrustRight(InputAction.CallbackContext context)
-    {
-        this.thrustVelocity.x = context.ReadValue<float>();
-    }
-    public void OnThrustUp(InputAction.CallbackContext context)
-    {
-        this.thrustVelocity.y = context.ReadValue<float>();
-    }
-    public void OnTurnPitchYaw(InputAction.CallbackContext context)
-    {
-        Vector2 input = context.ReadValue<Vector2>();
-        this.turnVelocity = new Vector3(-input.y * this.pitchYawSensitivity, input.x * this.pitchYawSensitivity, this.turnVelocity.z);
-    }
-    public void OnTurnRoll(InputAction.CallbackContext context)
-    {
-        this.turnVelocity.z = -context.ReadValue<float>() * this.rollSensitivity;
-    }
-
-    public void OnUnpossesed()
-    {
-        this.thrustVelocity = Vector3.zero;
-        this.turnVelocity = Vector3.zero;
-    }
-
-    void HandleThrust()
-    {
-        this.rgbd.velocity = this.transform.TransformDirection(Vector3.Scale(this.thrustVelocity, this.thurstSpeeds) * Time.deltaTime);
-    }
-
-    void HandleTurn()
-    {
-        this.transform.Rotate(this.turnVelocity * Time.fixedDeltaTime);
     }
 
     public void OnExitVehicle(InputAction.CallbackContext context)
     {
         this.controllable.Unpossess();
-        this.OnUnpossesed(); // Consider moving this to a controllable subscription.
+        this.spaceship.OnFullStop(); // Consider moving this to a controllable subscription.
     }
 }
+
+#endif
