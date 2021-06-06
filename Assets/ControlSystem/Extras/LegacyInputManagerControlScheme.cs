a7bea8d0efabd0942598a0eda894395f33d916d7
Dnelre-Slin <nilsheggem@hotmail.com>
2021-06-06 02:57:11 +0200

    Added legacy input manager control scheme

--- Assets/ControlSystem/Extras/LegacyInputManagerControlScheme.cs
+++ Assets/ControlSystem/Extras/LegacyInputManagerControlScheme.cs
@@ -1,20 +1,54 @@
 ﻿#if (ENABLE_LEGACY_INPUT_MANAGER)
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LegacyInputManagerControlScheme : MonoBehaviour, IControllScheme
+public class LegacyInputManagerControlScheme : MonoBehaviour, IControlScheme
 {
-    // Start is called before the first frame update
-    void Start()
+    private bool _controlsEnabled = false;
+    public bool controlsEnabled
     {
-
+        get { return this._controlsEnabled; }
     }
 
-    // Update is called once per frame
+    private List<LegacyInputTypeRawAxis> rawAxisInputs = new List<LegacyInputTypeRawAxis>();
+    private List<LegacyInputTypeKeyDown> keyDownInputs = new List<LegacyInputTypeKeyDown>();
+
+    void AddRawAxisInput(string axisX, string axisY, Action<Vector2> callback)
+    {
+        this.rawAxisInputs.Add(new LegacyInputTypeRawAxis(axisX, axisY, callback));
+    }
+
+    void AddKeyDownInput(string key, Action callback)
+    {
+        this.keyDownInputs.Add(new LegacyInputTypeKeyDown(key, callback));
+    }
+
     void Update()
     {
+        foreach (var rawAxis in this.rawAxisInputs)
+        {
+            Vector2 vec2 = new Vector2(Input.GetAxisRaw(rawAxis.axisX), Input.GetAxisRaw(rawAxis.axisY));
+            rawAxis.callback(vec2);
+        }
+        foreach (var keyDown in this.keyDownInputs)
+        {
+            if (Input.GetKeyDown(keyDown.key))
+            {
+                keyDown.callback();
+            }
+        }
+    }
 
+    public void Enable()
+    {
+        this._controlsEnabled = true;
+    }
+
+    public void Disable()
+    {
+        this._controlsEnabled = false;
     }
 }
 
---
+++ Assets/ControlSystem/Extras/LegacyInputTypes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct LegacyInputTypeRawAxis
+{
+    public string axisX;
+    public string axisY;
+    public Action<Vector2> callback;
+
+    public LegacyInputTypeRawAxis(string axisX, string axisY, Action<Vector2> callback)
+    {
+        this.axisX = axisX;
+        this.axisY = axisY;
+        this.callback = callback;
+    }
+}
+
+public struct LegacyInputTypeKeyDown
+{
+    public string key;
+    public Action callback;
+
+    public LegacyInputTypeKeyDown(string key, Action callback)
+    {
+        this.key = key;
+        this.callback = callback;
+    }
+}
